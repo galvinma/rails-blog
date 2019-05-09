601589daa54da0028d8dfd101f18cd6eeff65de3
Matthew Galvin <mattgalvin47@gmail.com>
2019-05-09 11:46:22 -0700

    Added comment functionality and basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # root 'welcome#index' tells Rails to map requests to the root of the application to the welcome controller's index action and get 'welcome/index' tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action.
   get 'welcome/index'
 
-  resources :articles
-  
+  resources :articles do
+    resources :comments
+  end
+
   root 'welcome#index'
 end
